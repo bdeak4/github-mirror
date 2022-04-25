d6d5769203f0eb033b16645928be1311f24ae582
Bartol Deak <b@bdeak.net>
2022-04-25 20:13:27 +0200

    rm empty directories

--- sync.sh
+++ sync.sh
@@ -2,13 +2,18 @@
 
 set -e
 
+REPOS_PATH=${REPOS_PATH:-./git}
+
+mkdir -p $REPOS_PATH/public
+mkdir -p $REPOS_PATH/private
+
 curl -s "https://$GITHUB_USER:$GITHUB_TOKEN@api.github.com/user/repos" |
 jq -r ".[] | select(.owner.login == \"$GITHUB_USER\") | [.private, .name, .ssh_url] | @tsv" |
 while read repo; do
 	is_private=$(echo $repo | awk '{print $1}')
 	name=$(echo $repo | awk '{print $2}')	
 	clone_url=$(echo $repo | awk '{print $3}')
-	path="./repositories/public/$name"
+	path="$REPOS_PATH/public/$name"
 
 	if [ "$is_private" = "true" ]; then
 		path=$(printf $path | sed 's|/public/|/private/|')
