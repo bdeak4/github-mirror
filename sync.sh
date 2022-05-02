00a12c16f941e7a6c1eadec47c206b36f4db702a
Bartol Deak <b@bdeak.net>
2022-05-02 02:13:16 +0200

    add github repository description and update idle time

--- sync.sh
+++ sync.sh
@@ -8,22 +8,26 @@ mkdir -p $REPOS_PATH/public
 mkdir -p $REPOS_PATH/private
 
 curl -s "https://$GITHUB_USER:$GITHUB_TOKEN@api.github.com/user/repos" |
-jq -r ".[] | select(.owner.login == \"$GITHUB_USER\") | [.private, .name, .ssh_url] | @tsv" |
-while read repo; do
-	is_private=$(echo $repo | awk '{print $1}')
-	name=$(echo $repo | awk '{print $2}')	
-	clone_url=$(echo $repo | awk '{print $3}')
-	path="$REPOS_PATH/public/$name"
+jq -r ".[] | select(.owner.login == \"$GITHUB_USER\") | @base64" |
+while read row; do
+	_jq() {
+		echo $row | base64 --decode | jq -r $1
+	}
 
-	if [ "$is_private" = "true" ]; then
+	path=$REPOS_PATH/public/$(_jq .name)
+	if [ "$(_jq '.private')" = "true" ]; then
 		path=$(printf $path | sed 's|/public/|/private/|')
 	fi
 
-	echo $path
-
 	if [ -d "$path" ]; then
-		git -C "$path" fetch --all
+		git -C $path fetch --all --quiet
 	else
-		git clone --mirror "$clone_url" "$path"
+		git clone --mirror $(_jq '.clone_url') $path
 	fi
+
+	mkdir -p $path/info/web
+	git -C $path for-each-ref --sort=-authordate --count=1 \
+		--format='%(authordate:iso8601)' > $path/info/web/last-modified
+
+	_jq '.description' | sed 's/^null$//g' > $path/description
 done
